7f755c7763fa37e67658684fb2e2b7fe19cf2947
Tanel Lebedev <tanel.lebedev@gmail.com>
2015-07-29 15:00:31 +0200

    TimelineWindowController (osx)

---
+++ src/ui/osx/TogglDesktop/test2/TimelineWindowController.h
@@ -0,0 +1,12 @@
+//
+//  TimelineWindowController.h
+//  TogglDesktop
+//
+//  Copyright (c) 2015 Toggl Desktop Developers. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface TimelineWindowController : NSWindowController
+
+@end
---
+++ src/ui/osx/TogglDesktop/test2/TimelineWindowController.m
@@ -0,0 +1,22 @@
+//
+//  TimelineWindowController.m
+//  TogglDesktop
+//
+//  Copyright (c) 2015 Toggl Desktop Developers. All rights reserved.
+//
+
+#import "TimelineWindowController.h"
+
+@interface TimelineWindowController ()
+
+@end
+
+@implementation TimelineWindowController
+
+- (void)windowDidLoad {
+    [super windowDidLoad];
+    
+    // Implement this method to handle any initialization after your window controller's window has been loaded from its nib file.
+}
+
+@end
